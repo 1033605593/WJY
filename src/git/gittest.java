01db487dfb9fad150971791a4655f8f81269f6b4
wjywjy <1033605593@qq.com>
2018-06-22 20:30:41 +0800

    test

--- src/git/gittest.java
+++ src/git/gittest.java
@@ -1,11 +1,9 @@
 package git;
 
-public class git {
-
+public class gittest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("git");
+		System.out.println("gittest");
 
 	}
-
 }
